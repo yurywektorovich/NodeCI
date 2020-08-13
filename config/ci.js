f1d4ab07cf112196d14ddd6212cb879571c4783b
Yury Gorelik <yurywektorovich@gmail.com>
2020-08-13 16:52:42 +0300

    travis-fix

--- config/ci.js
+++ config/ci.js
@@ -4,5 +4,5 @@ module.exports = {
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI: "mongodb://localhost:27017/blog_ci",
   cookieKey: "123123123",
-  redisUrl: "redis://127:0.0.1:6379",
+  redisUrl: "redis://localhost:6379",
 };
